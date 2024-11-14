6995950de1768c306bf6c0ace737c91cdf18d632
jkowskib <ben@jkowski.com>
2024-11-13 20:43:27 -0600

    Updated tests

---
+++ src/tests/ExpressionTests.java
@@ -0,0 +1,19 @@
+package tests;
+
+import jkowski.arithmocha.Expression;
+
+public class ExpressionTests {
+    public static void main(String[] args) {
+        System.out.println(
+                Expression.evaluate("10 + 2 + 7 * 15")
+        ); // 117.0
+
+        System.out.println(
+                Expression.evaluate("10 * (5 + 5) - (4 + 1)")
+        ); // 95.0
+
+        System.out.println(
+                Expression.evaluate("5 + (4 + (3 + (2 + 1)))")
+        ); // 15.0
+    }
+}
