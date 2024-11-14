2415d7dc63817c92da950541f1bd47a29bba0601
jkowskib <ben@jkowski.com>
2024-11-13 21:30:27 -0600

    Updated FunctionTests.java to reflect changes

--- src/tests/FunctionTests.java
+++ src/tests/FunctionTests.java
@@ -6,10 +6,10 @@ import jkowski.arithmocha.Function;
 public class FunctionTests {
     public static void main(String[] args) {
         Function pythagoreanFormula = new Function(
-                "(a^2) + (b^2)", "a", "b"
+                "pythagoreanFormula", "(a^2) + (b^2)", "a", "b"
         );
 
-        System.out.println(pythagoreanFormula); // f(a, b) = (a^2) + (b^2)
+        System.out.println(pythagoreanFormula); // pythagoreanFormula(a, b) = (a^2) + (b^2)
 
         String compiledFormula = pythagoreanFormula.compile(
                 2, 4 // a, b
