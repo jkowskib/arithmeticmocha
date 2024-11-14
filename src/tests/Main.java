8fbb3d7592bc21f955abeeb7610535cd0e6b89dd
jkowskib <ben@jkowski.com>
2024-11-13 19:45:20 -0600

    Removed unused Args in test

--- src/tests/Main.java
+++ src/tests/Main.java
@@ -3,7 +3,7 @@ package tests;
 import jkowski.arithmocha.Expression;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main() {
         double result = Expression.evaluate("10 + 2 + 7 * 15");
         System.out.println(result);
     }
